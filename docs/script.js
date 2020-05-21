ba799717f62cfa13fc09a9f347dad704c4e79046
Xavier Foucrier <xavier.foucrier@gmail.com>
2020-05-21 18:44:56 +0200

    Add missing script for @barba/css transitions

--- docs/script.js
+++ docs/script.js
@@ -4,6 +4,21 @@ let script = document.createElement('script');
 
 script.onload = () => {
   new ZeroClick();
+
+  barba.hooks.enter(() => {
+    new ZeroClick();
+  });
+
+  barba.use(barbaCss);
+
+  barba.init({
+    preventRunning: true,
+    transitions: [{
+      name: 'fade',
+      once() {},
+      leave() {}
+    }]
+  });
 };
 
 // load the appropriate library (based on environment)
