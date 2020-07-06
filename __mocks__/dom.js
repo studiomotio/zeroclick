71246c7745305a3073ead6b3bcbcab3c6297da04
Xavier Foucrier <xavier.foucrier@gmail.com>
2020-07-06 17:24:18 +0200

    Mock the keyboard `Enter` event

--- __mocks__/dom.js
+++ __mocks__/dom.js
@@ -2,13 +2,17 @@ export function init() {
   const mouseenterEvent = new MouseEvent('mouseenter');
   const mouseleaveEvent = new MouseEvent('mouseleave');
   const clickEvent = new MouseEvent('click');
+  const keydownEnterEvent = new KeyboardEvent('keydown', {
+    code: 'Enter',
+    key: 'Enter'
+  });
 
   const link = document.createElement('a');
   link.href = '/';
 
   document.body.appendChild(link);
 
-  return { link, mouseenterEvent, mouseleaveEvent, clickEvent };
+  return { link, mouseenterEvent, mouseleaveEvent, clickEvent, keydownEnterEvent };
 }
 
 export function clear() {
