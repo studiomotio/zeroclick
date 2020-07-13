08027a9a6ea7ce35a8bd88c1997bea64a801c269
Xavier Foucrier <xavier.foucrier@gmail.com>
2020-07-13 23:44:04 +0200

    Drop terser as it is used by default in webpack 4 production build

--- webpack.docs.js
+++ webpack.docs.js
@@ -1,14 +1,7 @@
 'use strict';
 
 const { merge } = require('webpack-merge');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = () => merge(require('./webpack.common.js')(), {
-  mode: 'production',
-  watch: false,
-  optimization: {
-    minimizer: [
-      new TerserPlugin()
-    ]
-  },
+  mode: 'production'
 });
