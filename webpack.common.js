61ab14b405f8f0b41329c71b42a90cdc9abaee4e
Xavier Foucrier <xavier.foucrier@gmail.com>
2020-07-15 00:00:48 +0200

    Use sass with autoprefixer to build docs stylesheet

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const path = require('path');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = () => ({
   entry: [
     './src/zeroclick.js',
-    './docs/src/index.js'
+    './docs/src/index.js',
+    './docs/src/index.scss'
   ],
   output: {
     path: path.resolve(__dirname, 'docs'),
@@ -18,9 +21,39 @@ module.exports = () => ({
   },
   module: {
     rules: [{
+      test: /\.woff$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[folder]/[name].[ext]',
+          emitFile: false
+        }
+      }
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader
+      }, {
+        loader: 'css-loader'
+      }, {
+        loader: 'postcss-loader',
+        options: {
+          plugins: () => [
+            autoprefixer()
+          ]
+        }
+      }, {
+        loader: 'sass-loader'
+      }]
+    }, {
       test: /\.js$/,
       use: 'babel-loader',
       exclude: /node_modules/
     }]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'assets/app.css'
+    })
+  ]
 });
