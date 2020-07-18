3f6b9484cec2fa192421028512b537bc2cf5d2da
Xavier Foucrier <xavier.foucrier@gmail.com>
2020-07-18 23:34:41 +0200

    Publish UMD build for the docs site

--- webpack.docs.js
+++ webpack.docs.js
@@ -3,5 +3,8 @@
 const { merge } = require('webpack-merge');
 
 module.exports = () => merge(require('./webpack.common.js')(), {
-  mode: 'production'
+  mode: 'production',
+  output: {
+    libraryTarget: 'umd'
+  }
 });
