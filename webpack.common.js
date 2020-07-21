18b6cf21e994be58ff7f2c0a42bebf3aeea463cc
Xavier Foucrier <xavier.foucrier@gmail.com>
2020-07-21 22:34:45 +0200

    Let microbundle transpile the package and use preset-env for docs bundle

--- webpack.common.js
+++ webpack.common.js
@@ -47,8 +47,15 @@ module.exports = () => ({
       }]
     }, {
       test: /\.js$/,
-      use: 'babel-loader',
-      exclude: /node_modules/
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env'
+          ]
+        }
+      }
     }]
   },
   plugins: [
