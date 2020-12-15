c0e68f4baa86bc4328ee0580d9bd7386e6aed0dd
Xavier Foucrier <xavier.foucrier@gmail.com>
2020-12-15 23:39:29 +0100

    Use gsap to animate the progress circle with zeroclick

--- docs/src/index.js
+++ docs/src/index.js
@@ -2,9 +2,54 @@ import zeroclick from 'source/zeroclick.js';
 import barba from '@barba/core';
 import gsap from 'gsap';
 
+let progress = document.querySelector('.progress');
+let circle = document.querySelector('.progress-circle');
+let length = circle.getTotalLength();
+let timeline = gsap.timeline();
+
+gsap.set(progress, {
+  strokeDasharray: length,
+  strokeDashoffset: length,
+});
 
 zeroclick.init({
   on: 'a:not(.github-button)',
+  await(resolve) {
+    timeline.clear().to(progress, {
+      rotation: 360,
+      strokeDashoffset: 0,
+      strokeDasharray: length,
+      duration: 1.2,
+      onComplete: () => {
+        resolve();
+      },
+    }).to(progress, {
+      opacity: 1,
+      duration: 0.4,
+    }, '<');
+  },
+  onCancel() {
+    timeline.clear().to(progress, {
+      opacity: 0,
+      rotation: 0,
+      strokeDashoffset: length,
+      duration: 0.4,
+    });
+  },
+  onDispatch() {
+    timeline.clear().to(progress, {
+      opacity: 0,
+      duration: 0.2,
+      delay: 0.1,
+      onComplete: () => {
+        gsap.set(progress, {
+          rotation: 0,
+          strokeDasharray: length,
+          strokeDashoffset: length,
+        });
+      },
+    });
+  },
 });
 
 barba.hooks.enter(() => {
