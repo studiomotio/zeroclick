cfb2102ce30f9f79e80ad8f1806ab9c74a0fb1e2
Xavier Foucrier <xavier.foucrier@gmail.com>
2020-12-17 23:42:02 +0100

    Improve webpack docs configuration (draft)

--- webpack.docs.js
+++ webpack.docs.js
@@ -1,5 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const pack = require('./package.json');
 const { merge } = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+// package preamble
+const preamble = `/*!\n  ${pack.name} – ${pack.description}\n  ${pack.author.name} ${pack.author.github} ${new Date().getFullYear()} ${pack.license}\n  ${pack.version}\n*/`;
 
 module.exports = () => merge(require('./webpack.common.js')(), {
   mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          output: {
+            comments: false,
+            preamble: preamble,
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new OptimizeCSSAssetsPlugin({
+      cssProcessorPluginOptions: {
+        preset: ['default', {
+          discardComments: {
+            removeAll: true,
+          },
+        }],
+      },
+    }),
+    new webpack.BannerPlugin({
+      raw: true,
+      banner: () => {
+        return preamble;
+      },
+    }),
+  ],
 });
